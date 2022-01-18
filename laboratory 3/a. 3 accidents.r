efba553620101d65ff99c19213a10a9c9fac8417
laurencetroyv-hailan <laurencetroyv@gmail.com>
2022-01-18 19:36:09 +0800

    Laboratory 3-Test

---
+++ laboratory 3/a. 3 accidents.r
@@ -0,0 +1,2 @@
+# ! less than 3 accidents will occur? Ans. approximately 0.4232
+print(ppois(2, 3))
---
+++ laboratory 3/b. 2 accidents.r
@@ -0,0 +1,2 @@
+# ! at least 2 accidents will occur? Ans. approximately 0.8008
+print(ppois(1, 3, lower = FALSE))
